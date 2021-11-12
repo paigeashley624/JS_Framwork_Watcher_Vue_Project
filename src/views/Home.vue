d0bfce1cbfdf0570cd3de1c6bd3da4186742253c
Paige Morton <paigeashley624@gmail.com>
2021-11-11 21:06:35 -0500

    adds methods and prints to body what data is being pulled

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>
+      Welcome to Javascript Framework Watcher: Stars, Watchers, and Forks https://github.com/acltc/js-framework-watcher
+    </h1>
+    <h2 v-for="info in repoData" :key="info.id">
+      {{ info.full_name }}
+      {{ info.forks_count }}
+    </h2>
   </div>
 </template>
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
-  data: function () {
+  data() {
     return {
-      message: "Welcome to Javascript Framework Watcher",
+      repoData: [],
     };
   },
-  created: function () {},
-  methods: {},
+  mounted() {
+    this.getRepo("https://api.github.com/repos/vuejs/vue");
+    this.getRepo("https://api.github.com/repos/angular/angular.js");
+  },
+  created() {},
+  methods: {
+    getRepo(url) {
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data);
+          this.repoData = [...this.repoData, response.data];
+          console.log(this.repoData);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.repoData = {};
+        });
+    },
+  },
 };
 </script>
