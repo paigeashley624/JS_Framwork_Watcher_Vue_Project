9a8e310a2a1c8ce4eeba7714bad768a543a55017
Paige Morton <paigeashley624@gmail.com>
2021-11-12 17:42:16 -0500

    adds testing template for tutorial

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,11 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       Welcome to Javascript Framework Watcher: Stars, Watchers, and Forks https://github.com/acltc/js-framework-watcher
     </h1>
     <h2 v-for="info in repoData" :key="info.id">
+      "info" could be named anything
       {{ info.full_name }}
       {{ info.forks_count }}
     </h2>
@@ -16,20 +17,21 @@ import axios from "axios";
 export default {
   data() {
     return {
-      repoData: [],
+      repoData: [], // this is an empty array until line 33 inserts information
     };
   },
   mounted() {
     this.getRepo("https://api.github.com/repos/vuejs/vue");
     this.getRepo("https://api.github.com/repos/angular/angular.js");
   },
-  created() {},
+
   methods: {
     getRepo(url) {
       axios
         .get(url)
         .then((response) => {
-          console.log(response.data);
+          //response is whaever data the api returns
+          console.log(response.data); //.data
           this.repoData = [...this.repoData, response.data];
           console.log(this.repoData);
         })
